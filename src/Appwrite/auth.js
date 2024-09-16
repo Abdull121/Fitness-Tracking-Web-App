ff3cea690d3d806fb82e9e717101e9e05dbe2564
Muhammad Abdullah <93944428+Abdull121@users.noreply.github.com>
2024-09-16 05:09:29 +0500

    protectedRoutes

---
+++ src/Appwrite/auth.js
@@ -0,0 +1,72 @@
+import conf from '../conf/Conf';
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+        
+        // console.log("appwriteURL :: ",conf.appwriteUrl)
+        // console.log("Project ID :: ",conf.appwriteProjectId)
+        // console.log("Collection ID :: ",conf.appwriteCollectionId)
+        // console.log( "BucketID :: ",conf.appwriteBucketId)
+        // console.log("DatabaseID:: ",conf.appwriteDatabaseId)
+
+
+            
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
+            console.log("createAccount APPWRITE Error:: ",error)
+           
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            console.log("Login APPWRITE Error:: ",error)
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+
+export default authService
+
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,44 +1,69 @@
 import React from 'react';
-import {Header, Footer, SideBar} from './components/index'
-import { Outlet, Navigate} from 'react-router-dom';
+import { Header, Footer, SideBar } from './components/index';
+import { Outlet, useNavigate } from 'react-router-dom';
+import authService from './Appwrite/auth';
+import { Loader } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
+
+/**
+ * Layout component that handles the main layout of the application.
+ * It includes the Header, SideBar, and Footer components.
+ * It also manages the loading state and user authentication.
+ */
 
 function Layout() {
   const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  // const [user, setUser] = React.useState(null);
+  const navigate = useNavigate();
+
+   /**
+   * Toggles the sidebar open and close state.
+   * @returns {void}
+   */
 
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
 
+  React.useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      // const data= true
+      if (userData) {
+        // setUser(userData);
+        navigate('/userprofile');
+      } else {
+        navigate('/login');
+      }
+      setLoading(false);
+    }).catch(() => {
+      navigate('/login');
+      setLoading(false);
+    });
+  }, []);
 
-  const user = true;
 
+  // loader 
+  if (loading) {
+    return (
+    <div className='flex justify-center items-center' style={{ height: '100vh' }}>
+        <Loader size="md" />
+    </div>
+    )
+    
+    
+  }
 
-  return (user ? 
-  <>
-    <Header isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <div className="md:flex bg-gray-100 md:pr-5">
-          <SideBar isSidebarOpen={isSidebarOpen} />
-          <div className="md:flex-1">
-            <Outlet />
-           </div>
-       </div>
-        <Footer />
-  </>:
-   <Navigate to="/login"/>)
-
-
-  // return (
-     
-  //   <>
-      
-  //     <Header isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-  //     <div className="md:flex bg-gray-100 md:pr-5">
-  //       <SideBar isSidebarOpen={isSidebarOpen} />
-  //       <div className="md:flex-1">
-  //         <Outlet />
-  //       </div>
-  //     </div>
-  //     <Footer />
-  //   </>
-  // );
+  return (
+    <>
+      <Header isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className="md:flex bg-gray-100 md:pr-5">
+        <SideBar isSidebarOpen={isSidebarOpen} />
+        <div className="md:flex-1">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
