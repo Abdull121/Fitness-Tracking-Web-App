faef9ca03488e5268ac636c2f8811480f3657935
Muhammad Abdullah <93944428+Abdull121@users.noreply.github.com>
2024-10-18 18:17:48 +0500

    fixed positions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,83 +1,36 @@
-import React from 'react';
-import { Dumbbell, Menu, X, UserRound } from 'lucide-react';
-import { Link, NavLink } from 'react-router-dom';
+import React from "react";
+import { Dumbbell, Menu, X, UserRound } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function Header({ isSidebarOpen, toggleSidebar }) {
   return (
-    
-      <header className="bg-indigo-600 text-white shadow-lg">
-        <div className="max-w-10xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-          <div className="flex items-center">
-            <button onClick={toggleSidebar} className=" absolute right-0 4 mr-2 md:hidden">
-              {isSidebarOpen ? <X size={24} /> : <Menu size={24} />}
-            </button>
-            <Dumbbell className="h-8 w-8 mr-2" />
-            <h1 className="text-2xl font-bold">Fitness World</h1>
-          </div>
-
-
-
-              <nav className="hidden md:flex space-x-4">
-    
-          {/* <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `hover:text-white transition-colors duration-200 ${
-                isActive ? 'text-white' : 'text-indigo-100'
-              }`
-            }
+    <header className="bg-indigo-600 text-white shadow-lg">
+      <div className="max-w-10xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
+        <div className="flex items-center">
+          <button
+            onClick={toggleSidebar}
+            className=" absolute right-0 4 mr-2 md:hidden"
           >
-            Dashboard
-          </NavLink>
-          <NavLink
-            to="/workout"
-            className={({ isActive }) =>
-              `hover:text-white transition-colors duration-200 ${
-                isActive ? 'text-white' : 'text-indigo-100'
-              }`
-            }
-          >
-            Workouts
-          </NavLink>
-          <NavLink
-            to="/progress"
-            className={({ isActive }) =>
-              `hover:text-white transition-colors duration-200 ${
-                isActive ? 'text-white' : 'text-indigo-100'
-              }`
-            }
-          >
-            Progress
-          </NavLink>
-          <NavLink
-            to="/history"
-            className={({ isActive }) =>
-              `hover:text-white transition-colors duration-200 ${
-                isActive ? 'text-white' : 'text-indigo-100'
-              }`
-            }
-          >
-            History
-          </NavLink> */}
+            {isSidebarOpen ? (
+              <X className="mr-1.5" size={24} />
+            ) : (
+              <Menu className="mr-1.5" size={24} />
+            )}
+          </button>
+          <Dumbbell className="h-8 w-8 mr-2" />
+          <h1 className="text-2xl font-bold">Fitness World</h1>
+        </div>
 
-
+        <nav className="hidden md:flex space-x-4">
           <Link to="/userprofile">
             <div className="ml-5 bg-blue-100 rounded-full p-2 hover:bg-blue-200 transition duration-300">
-                <UserRound className="w-8 h-8 text-blue-700 hover:text-blue-800"/>
+              <UserRound className="w-8 h-8 text-blue-700 hover:text-blue-800" />
             </div>
           </Link>
-
-
-            </nav>
+        </nav>
       </div>
-      </header>
-
-
-
+    </header>
   );
-  
 }
 
 export default Header;
-
-
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from 'react'
-import { useForm} from 'react-hook-form'
-import { FcGoogle } from 'react-icons/fc'
-import { FiEye, FiEyeOff } from 'react-icons/fi'
-import Input from './Input'
-import { Link ,useNavigate} from 'react-router-dom'
-import authService from '../Appwrite/auth'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import Input from "./Input";
+import { Link, useNavigate } from "react-router-dom";
+import authService from "../Appwrite/auth";
 
-
-import { Dumbbell} from 'lucide-react';
-
+import { Dumbbell } from "lucide-react";
 
 export default function SignInForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm()
-  const [showPassword, setShowPassword] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-   const [error, setError] = useState("")
+  const [error, setError] = useState("");
 
-   const onSubmit = async (data) => {
+  const onSubmit = async (data) => {
     setError(""); // Clear any previous error messages
-  
+
     try {
       // console.log('Sign in attempt:', data);
-  
-      const session = await authService.login(data); 
+
+      const session = await authService.login(data);
 
       //console.log("sessions:: ", session)
-  
+
       if (session) {
         const userData = await authService.getCurrentUser();
-       // console.log("user login response:: ",userData)
-        
+        // console.log("user login response:: ",userData)
+
         if (userData) {
           // Navigate to the dashboard if user data exists
           navigate("/");
@@ -46,7 +48,7 @@ export default function SignInForm() {
     } catch (error) {
       // Handle any errors from the login attempt
       console.log("login error:", error);
-      
+
       if (error.message.includes("User not found")) {
         setError("User not found");
       } else {
@@ -55,23 +57,14 @@ export default function SignInForm() {
     }
   };
 
-
-
-  
-  
-
-
-
-  const handleGoogleAuth =  async ()=>{
-
+  const handleGoogleAuth = async () => {
     try {
-
-      self.addEventListener('fetch', (event) => {
+      self.addEventListener("fetch", (event) => {
         // Ignore requests with unsupported schemes like 'chrome-extension://'
-        if (event.request.url.startsWith('chrome-extension://')) {
+        if (event.request.url.startsWith("chrome-extension://")) {
           return; // Skip caching for this request
         }
-        
+
         // Proceed with the caching logic for other requests
         event.respondWith(
           caches.match(event.request).then((response) => {
@@ -79,66 +72,59 @@ export default function SignInForm() {
           })
         );
       });
-      await authService.googleAuth()
-      
-      
-     
-      
+      await authService.googleAuth();
     } catch (error) {
-      console.log(error)
-      
+      console.log(error);
     }
-
-  }
-  
-
-
-  
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-      <div className='flex justify-center items-center'>
+        <div className="flex justify-center items-center">
           <Dumbbell className="h-14 w-14 mr-3 text-blue-600" />
           <h1 className="text-4xl font-bold text-blue-600">Fitness World</h1>
         </div>
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign In</h2>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign In
+          </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Stay updated on your fitness journey
           </p>
         </div>
         <div className="text-center text-red-600">
-  {error && <p>{error}</p>}
+          {error && <p>{error}</p>}
         </div>
 
         <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
           <div className="rounded-md shadow-sm space-y-4">
             <div>
-
-              <Input 
+              <Input
                 label="Email: "
-                 placeholder="Enter your email"
+                placeholder="Enter your email"
                 type="email"
                 autoComplete="email"
                 {...register("email", {
-                    required: "Email is Required",
-                    validate: {
-                        matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                        "Email address must be a valid address",
-                    }
+                  required: "Email is Required",
+                  validate: {
+                    matchPatern: (value) =>
+                      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+                        value
+                      ) || "Email address must be a valid address",
+                  },
                 })}
-
                 className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-              
               />
-              {errors.email && <p className="mt-2 text-sm text-red-600">{errors.email.message}</p>}
+              {errors.email && (
+                <p className="mt-2 text-sm text-red-600">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
 
-
             <div className="relative">
-              
-              <Input 
+              <Input
                 label="password"
                 id="password"
                 name="password"
@@ -153,15 +139,26 @@ export default function SignInForm() {
                 className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
                 onClick={() => setShowPassword(!showPassword)}
               >
-                {showPassword ? <FiEyeOff className="h-5 w-5 text-gray-500 absolute right-4 top-9 z-10" /> : <FiEye className="h-5 w-5 text-gray-500 absolute right-4 top-9 z-10" />}
+                {showPassword ? (
+                  <FiEyeOff className="h-5 w-5 text-gray-500 absolute right-4 top-9 z-10" />
+                ) : (
+                  <FiEye className="h-5 w-5 text-gray-500 absolute right-4 top-9 z-10" />
+                )}
               </button>
-              {errors.password && <p className="mt-2 text-sm text-red-600">{errors.password.message}</p>}
+              {errors.password && (
+                <p className="mt-2 text-sm text-red-600">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
           </div>
 
           <div className="flex items-center justify-between">
             <div className="text-sm">
-              <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
+              <a
+                href="#"
+                className="font-medium text-blue-600 hover:text-blue-500"
+              >
                 Forgot password?
               </a>
             </div>
@@ -183,7 +180,9 @@ export default function SignInForm() {
               <div className="w-full border-t border-gray-300"></div>
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-gray-50 text-gray-500">Or continue with</span>
+              <span className="px-2 bg-gray-50 text-gray-500">
+                Or continue with
+              </span>
             </div>
           </div>
 
@@ -201,13 +200,16 @@ export default function SignInForm() {
 
         <div className="text-center">
           <p className="mt-2 text-sm text-gray-600">
-            New to FitnessApp?{' '}
-            <Link to="/signup" className="font-medium text-blue-600 hover:text-blue-500">
+            New to FitnessApp?{" "}
+            <Link
+              to="/signup"
+              className="font-medium text-blue-600 hover:text-blue-500"
+            >
               Join now
             </Link>
           </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
