97e959196a312c170c0d655fe6cd0625f2755f39
Muhammad Abdullah <93944428+Abdull121@users.noreply.github.com>
2024-10-22 04:46:15 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,44 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import { Dashboard } from "./components/index.js";
+import Workouts from "./pages/Workouts.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
+import Progress from "./pages/Progress.jsx";
+import History from "./pages/History.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
 
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
-import Layout from './Layout.jsx'
-import {Dashboard} from './components/index.js'
-import Workouts from './pages/Workouts.jsx'
-import UserProfile from './pages/UserProfile.jsx'
-import Progress from './pages/Progress.jsx'
-import History from './pages/History.jsx'
-import Login from './pages/Login.jsx'
-import SignUp from './pages/SignUp.jsx'
-
-
-const router =  createBrowserRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
-    
     <Route>
-     <Route path='login' element={<Login/>}/>
-     <Route path='signup' element={<SignUp/>}/>
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
 
-      <Route path='/' element={<Layout/>}>
-
-          <Route path='dashboard' element={<Dashboard/>} />
-           <Route  path='userprofile'element={<UserProfile/>}/>
-          <Route path='workout' element={<Workouts/>}/> 
-          <Route path='progress' element={<Progress/>}/>
-          <Route path='history' element={<History/>}/>
-
-     </Route>
+      <Route path="/" element={<Layout />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="userprofile" element={<UserProfile />} />
+        <Route path="workout" element={<Workouts />} />
+        <Route path="progress" element={<Progress />} />
+        <Route path="history" element={<History />} />
+      </Route>
     </Route>
   )
-)
-
-
- 
-
+);
 
 function App() {
-
-  // const weeklyGoalsRef = useRef();
-
-  // const handleProgressUpdate = () => {
-  //   if (weeklyGoalsRef.current) {
-  //     weeklyGoalsRef.current.updateWeeklyGoals();
-  //   }
-  // };
   
 
   return (
     <>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
 
-    {/* <div className="App">
-      <DailyGoals onProgressUpdate={handleProgressUpdate} />
-      <GoalsProgress ref={weeklyGoalsRef} />
-    </div> */}
-
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
